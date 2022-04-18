b32a13ad038948df2d9ee2acf24a4df421752215
nova chandra kirana <101784704+novachan10@users.noreply.github.com>
2022-04-19 05:23:34 +0700

    menambahkan file code Bangun Datar.java

---
+++ Class X/Semester 2/Inheritance.java/Latihan 4/Bangun Datar.java
@@ -0,0 +1,12 @@
+public class BangunDatar {
+    
+    public void luas(){
+        System.out.println("menghitung luas lingkaran");
+        
+    }
+    public void keliling(){
+        System.out.println("menghitung keliling lingkaran");
+        
+    }
+    
+}
