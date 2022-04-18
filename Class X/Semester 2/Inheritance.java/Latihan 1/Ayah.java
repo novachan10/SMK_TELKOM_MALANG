19e2b839eaafba2a707342590c0eb3ad50233682
nova chandra kirana <101784704+novachan10@users.noreply.github.com>
2022-04-19 05:13:31 +0700

    menambahkan file code Ayah.java

---
+++ Class X/Semester 2/Inheritance.java/Latihan 1/Ayah.java
@@ -0,0 +1,8 @@
+public class Ayah {
+    String Sifat= "Penyabar";
+    double TB= 173.1;
+    
+    void hobbi(){
+        System.out.println("Hobi nya adalah Ngo-piding=Ngopi dan Ngoding");
+    }
+}
