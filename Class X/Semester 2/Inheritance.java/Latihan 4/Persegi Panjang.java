07e538a2d8ed1e789e2ef8c9f2552cb835862318
nova chandra kirana <101784704+novachan10@users.noreply.github.com>
2022-04-19 05:24:33 +0700

    menambahkan file code Persegi Panjang.java

---
+++ Class X/Semester 2/Inheritance.java/Latihan 4/Persegi Panjang.java
@@ -0,0 +1,14 @@
+public class Persegipanjang extends BangunDatar {
+    float panjang,lebar;
+    
+    public void luas(){
+        float luas=panjang*lebar;
+        System.out.println("Luas Persegi panjang = " + luas);
+    }
+    
+    public void keliling(){
+        float keliling=2*(panjang+lebar);
+        System.out.println("Keliling Persegi panjang = " + keliling);
+    }
+    
+}
