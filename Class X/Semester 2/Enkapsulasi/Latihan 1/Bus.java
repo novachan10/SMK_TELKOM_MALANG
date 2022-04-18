5b1025a3a17c79ce922972d9ceb162a6dd0d1ff6
nova chandra kirana <101784704+novachan10@users.noreply.github.com>
2022-04-18 20:40:47 +0700

    Menambahkan file code Bus java

---
+++ Class X/Semester 2/Enkapsulasi/Latihan 1/Bus.java
@@ -0,0 +1,9 @@
+public class Bus {
+    public int penumpang,maxpenumpang;
+    
+    
+    public void cetak(){
+        System.out.println("Penumpang sekarang = "+penumpang);
+        System.out.println("penumpang seharusnya adalah ="+maxpenumpang);
+    }
+}
