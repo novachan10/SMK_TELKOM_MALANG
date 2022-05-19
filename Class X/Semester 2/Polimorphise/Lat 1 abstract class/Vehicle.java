b5ccc83b5780fc9209f22061b1341dcbcdbca3d6
nova chandra kirana <101784704+novachan10@users.noreply.github.com>
2022-05-19 08:16:59 +0700

    menambahkan file coding java

---
+++ Class X/Semester 2/Polimorphise/Lat 1 abstract class/Vehicle.java
@@ -0,0 +1,10 @@
+public abstract class Vehicle {
+    public void function(){
+        System.out.println("Tools transportation");
+    }
+    public void fuel(){
+        System.out.println("fuels");
+    }
+    public abstract void walk(); // penulisan method abstract    
+    
+}
